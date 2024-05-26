a11371f23a339a3334790c8bd66c6e41aacc45ba
Abdulquddus Abdulrahman (Lexzee) <developerlexzee@gmail.com>
2024-05-26 01:37:59 +0100

    Added properties to properties page and Recent Properties in Homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '@/components/Hero'
 import InfoBoxes from '@/components/InfoBoxes'
+import HomeProperty from '@/components/HomeProperty'
 
 const Homepage = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperty />
     </>
   )
 }
