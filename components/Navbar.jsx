8d8773a4f906c56c1cdc497e1a7e152f37efca60
Abdulquddus Abdulrahman (Lexzee) <developerlexzee@gmail.com>
2024-05-31 14:36:35 +0100

    signin/signup added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,13 +6,28 @@ import profileDefault from "@/assets/images/profile.png"
 import Link from "next/link"
 import { usePathname } from "next/navigation";
 import { FaGoogle } from 'react-icons/fa'
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Navbar = () => {
+  const { data: session } = useSession();
+
   const [isMobileMenu, setIsMobileMenu] = useState(false)
   const [isProfileMenu, setIsProfileMenu] = useState(false)
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [providers, setProviders] = useState(false)
   const pathname = usePathname()
+
+  useEffect(() => {
+    const setAuthProviders = async () => {
+      const res = await getProviders();
+
+      setProviders(res)
+    }
+
+    setAuthProviders()
+  }, [])
+
+  console.log(providers);
 
   return (
     <nav className="bg-blue-700 border-b border-blue-500">
@@ -68,14 +83,15 @@ const Navbar = () => {
                 <Link
                   href="/"
                   className={`${pathname === '/' ? 'bg-black' : ''} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
-                  >Home
+                >
+                  Home
                 </Link>
                 <Link
                   href="/properties"
                   className={`${pathname === '/properties' ? 'bg-black' : ''} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
                   >Properties
                 </Link>
-                {isLoggedIn && (
+                {session && (
                   <Link
                     href="/properties/add"
                     className={`${pathname === '/properties/add' ? 'bg-black' : ''} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
@@ -87,21 +103,25 @@ const Navbar = () => {
           </div>
 
           {/* <!-- Right Side Menu (Logged Out) --> */}
-          {!isLoggedIn && (
+          {!session && (
             <div className="hidden md:block md:ml-6">
               <div className="flex items-center">
-                <button
-                  className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                >
-                  <FaGoogle className="text-white mr-2" />
-                  <span>Login or Register</span>
-                </button>
+                {providers && Object.values(providers).map((provider, index) => (
+                  <button
+                    className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                    key={index}
+                    onClick={() => signIn(provider)}
+                  >
+                    <FaGoogle className="text-white mr-2" />
+                    <span>Login or Register</span>
+                  </button>
+                ))}
               </div>
             </div>
           )}
 
           {/* <!-- Right Side Menu (Logged In) --> */}
-          {isLoggedIn && (
+          {session && (
             <div
               className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0"
             >
@@ -210,7 +230,7 @@ const Navbar = () => {
               className={`${pathname === '/properties' ? 'bg-black' : ''} text-white hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium`}
               >Properties
             </Link>
-            {isLoggedIn && (
+            {session && (
               <Link
                 href="properties/add"
                 className={`${pathname === '/properties/add' ? 'bg-black' : ''} text-white hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium`}
@@ -218,14 +238,18 @@ const Navbar = () => {
               </Link>
             )}
 
-            {!isLoggedIn && (
-              <button
-                className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4"
-              >
-                <FaGoogle className="text-white mr-2" />
-                <span>Login or Register</span>
-              </button>
-            )}
+            {!session &&
+              providers && Object.values(providers).map((provider, index) => (
+                <button
+                  className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4"
+                  key={index}
+                  onClick={() => signIn(provider)}
+                >
+                  <FaGoogle className="text-white mr-2" />
+                  <span>Login or Register</span>
+                </button>
+              ))
+            }
           </div>
         </div>
       )}
--- models/User.js
+++ models/User.js
@@ -24,5 +24,5 @@ const UserSchema = new Schema({
   timestamps: true
 });
 
-const User = models.user || model('User', UserSchema)
+const User = models.User || model('User', UserSchema)
 export default User
